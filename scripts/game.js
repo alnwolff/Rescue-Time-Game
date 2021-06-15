1de5adcd46a9004b1a23a1ac83779aeadff28bce
Alan Sanchez <alnwolff@gmail.com>
2021-06-15 18:00:42 +0200

    correct the links for the assets

--- scripts/game.js
+++ scripts/game.js
@@ -14,19 +14,19 @@ class Game {
 
     preload() {
 		this.backgroundImages = [
-            { src: loadImage('../assets/background/layer07_Sky.png'), x: 0, speed: 0 },
-            { src: loadImage('../assets/background/layer06_Rocks.png'), x: 0, speed: 1 },
-            { src: loadImage('../assets/background/layer05_Clouds.png'), x: 0, speed: 2 },
-            { src: loadImage('../assets/background/layer04_Hills_2.png'), x: 0, speed: 3 },
-            { src: loadImage('../assets/background/layer03_Hills_1.png'), x: 0, speed: 4 },
-            { src: loadImage('../assets/background/layer02_Trees.png'), x: 0, speed: 5 },
-            { src: loadImage('../assets/background/layer01_Ground.png'), x: 0, speed: 6 },
+            { src: loadImage('assets/background/layer07_Sky.png'), x: 0, speed: 0 },
+            { src: loadImage('assets/background/layer06_Rocks.png'), x: 0, speed: 1 },
+            { src: loadImage('assets/background/layer05_Clouds.png'), x: 0, speed: 2 },
+            { src: loadImage('assets/background/layer04_Hills_2.png'), x: 0, speed: 3 },
+            { src: loadImage('assets/background/layer03_Hills_1.png'), x: 0, speed: 4 },
+            { src: loadImage('assets/background/layer02_Trees.png'), x: 0, speed: 5 },
+            { src: loadImage('assets/background/layer01_Ground.png'), x: 0, speed: 6 },
 		];
 
-        this.playerImage = loadImage('../assets/finn-walk.gif');
-        this.obstacleImage = loadImage('../assets/Hero-Bullet.gif');
-        this.cageImage = loadImage('../assets/cage.png')
-        this.prisionerImage = loadImage('../assets/jake-waiting-dance.gif')
+        this.playerImage = loadImage('assets/finn-walk.gif');
+        this.obstacleImage = loadImage('assets/Hero-Bullet.gif');
+        this.cageImage = loadImage('assets/cage.png')
+        this.prisionerImage = loadImage('assets/jake-waiting-dance.gif')
     }
 
     draw() {
